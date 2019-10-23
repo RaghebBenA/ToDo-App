fc25ccea827c56feda081dddd52ae5406304cab1
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-10-23 23:38:43 +0100

    Updates

---
+++ precache-manifest.fdd82119010f3bbe93d50756082f67f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1f3ddb06c89113faaba3cb66a83a7e",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "c6118d242b83de06b313",
+    "url": "/ToDo-App/static/css/main.068a7944.chunk.css"
+  },
+  {
+    "revision": "28bd0df3785dbac80f64",
+    "url": "/ToDo-App/static/js/2.b271ec92.chunk.js"
+  },
+  {
+    "revision": "c6118d242b83de06b313",
+    "url": "/ToDo-App/static/js/main.e7d03f24.chunk.js"
+  },
+  {
+    "revision": "4aa0236c74fcbb28e812",
+    "url": "/ToDo-App/static/js/runtime-main.028c3a1e.js"
+  }
+]);
